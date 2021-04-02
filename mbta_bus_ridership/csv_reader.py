a03d4b3e69989bb4cbd908b07266e0d1bc0a6af6
JBR <john.russell@tufts.edu>
2021-04-02 18:09:10 -0400

    Add test for CsvReader; fix bugs found whilst doing so

--- mbta_bus_ridership/csv_reader.py
+++ mbta_bus_ridership/csv_reader.py
@@ -8,25 +8,29 @@ class CsvReader:
     def read(self, file_path, delimiter=','):
         with open(file_path) as f:
             csv_reader = csv.reader(f, delimiter=delimiter)
-            columns = next(csv_reader)
-            unread_data = dataWithColumns(data=csv_reader, columns=self._create_csv_column_dict(columns))
-            data = self._read_in_data(unread_data)
+            column_row = next(csv_reader)
+            input_column_indices, output_column_indices = self._create_csv_column_dict(column_row)
+            data = self._read_in_data(csv_reader, input_column_indices, output_column_indices)
         return data
 
     @staticmethod
-    def _create_csv_column_dict(columns):
+    def _create_csv_column_dict(column_row):
         # Takes the first row of a CSV file (ie., the columns).  Returns dict(column_name: index_in_row).
-        column_index = 0
-        column_indices = dict()
-        for name in columns:
+        input_column_index = 0
+        output_column_index = 0
+        input_column_indices = dict()
+        output_column_indices = dict()
+        for name in column_row:
             if name:
-                column_indices[name] = column_index
-            column_index += 1
-        return column_indices
+                input_column_indices[name] = input_column_index
+                output_column_indices[name] = output_column_index
+                output_column_index += 1
+            input_column_index += 1
+        return input_column_indices, output_column_indices
 
     @staticmethod
-    def _read_in_data(data_with_columns):
-        columns = data_with_columns.columns
+    def _read_in_data(csv_reader, input_column_indices, output_column_indices):
+        columns = input_column_indices
         data_structure = namedtuple('MbtaData', [str(k) for k in columns.keys() if str(k)])
         return dataWithColumns([data_structure(**{col: line[columns[col]] for col in columns.keys() if col})
-                                for line in data_with_columns.data], columns)
+                                for line in csv_reader], output_column_indices)
